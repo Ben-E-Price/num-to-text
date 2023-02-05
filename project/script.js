44d391ef0c43293778bbf244ec3d51ec37d18852
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-02-05 16:33:18 +0000

    Object UiElements created Added id to input element UiElements object 	-References to UI elements 	-Will ultimately contain methods relating to UI functionality 	-Contains 3 keypairs 		-elInput - Stores "user-input" 		-elBtnSub - Stores "btn-convert" 		-elOutDis - Stores "output-display"
    HTML Changes
    	-Added id "user-input" to input element

---
+++ project/script.js
@@ -0,0 +1,8 @@
+"use strict"
+
+// Contains all UI elements
+const UiElements = {
+    elInput: document.getElementById("user-input"),
+    elBtnSub: document.getElementById("btn-convert"),
+    elOutDis: document.getElementById("output-display"),
+};
