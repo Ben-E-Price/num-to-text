1e8eadba2994b4e455e06b9c033a526a4cc9d64f
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-02-05 17:52:18 +0000

    stringsMap creation All strings required to construct output

--- project/script.js
+++ project/script.js
@@ -6,3 +6,48 @@ const UiElements = {
     elBtnSub: document.getElementById("btn-convert"),
     elOutDis: document.getElementById("output-display"),
 };
+
+// Contains strings used to construct output - Organized by place values
+const stringsMap = new Map([
+    //Single Values
+    [0, new Map([
+        [1, `one`],
+        [2, `two`],
+        [3, `three`],
+        [4, `four`],
+        [5, `five`],
+        [6, `six`],
+        [7, `seven`],
+        [8, `eight`],
+        [9, `nine`],
+    ])],
+
+    // Tens values 
+    [1, new Map([
+        [1, `teen`],
+        [2, `twenty`],
+        [3, `thirty`],
+        [4, `fourty`],
+        [5, `fifty`],
+        [6, `sixty`],
+        [7, `seventy`],
+        [8, `eighty`],
+        [9, `ninety`],
+
+        // Contains values that can not be constructed from other strings
+        [`unique`, new Map([
+            [10, `ten`],
+            [11, `eleven`],
+            [12, `twelve`],
+            [13, `thirteen`],
+            [15, `fifteen`],
+        ])],
+    ])],
+
+    [2, `hundred`]
+
+    [3, new Map([
+        [0, `thousand`],
+        [1, `million`],
+    ])]
+]);
